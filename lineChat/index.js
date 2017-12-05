c5f37c26f0937f679a70dd54ad2739b886146583
kao-chen-yu <ae6013@gmail.com>
2017-12-05 16:27:43 +0800

    test context

--- lineChat/index.js
+++ lineChat/index.js
@@ -6,11 +6,15 @@ const uuid = require('uuid/v1');
 var apiai = require('apiai');
 var app1 = apiai('df450cef4ea7467a8543a9c0ee587e2e');
 var fs=require('fs');
+
+
 var bot = linebot({
 	channelId : '1547763729',
 	channelSecret : '9e852ad5d789e81c1af1a51f6666d7c5',
 	channelAccessToken : 'i9WIA5CANkd5E9XjHYgRfq3DbPS1klBRTvBQRGKahHjZUrvunsYfibJRgnXisONeMXfZRqdYAg20GgQUDf6WB6l+XRTFUrSkpZ94cf3dcG7br0qX6vXihJ7gNFK0yt/aEGWfetUB9mTDTqv0Zrp/SwdB04t89/1O/w1cDnyilFU='
 });
+
+
 var singer='test';
 var check = false;
 bot.on('message', function(event) {
@@ -19,88 +23,95 @@ bot.on('message', function(event) {
     var msg = event.message.text;
 	var sessionid= uuid();
 	var speech='';
+	// put context
 	if(singer == 'test'){
-	var options = {
-		sessionId: uuid(),
-		contexts:[
-      {
-        "name": "find_singer-followup",
-        "parameters": {
-          "singer": "",
-          "singer.original": ""
-        }
-        
-      }
-    ]
-	};
+		var options = {
+			sessionId: uuid(),
+			contexts:[
+			  {
+				"name": "find_singer-followup",
+				"parameters": {
+					"singer": "",
+					"singer.original": ""
+				}		
+			  }
+			]	
+		};
 	}
 	else{
-	var options = {
-		sessionId: uuid(),
-		contexts:[
-      {
-        "name": "find_singer-followup",
-        "parameters": {
-          "singer": singer,
-          "singer.original": singer
-        }
-        
-      }
-    ]
-	};	
-		
-		
-	}
-	console.log('singers' + singer);
-	var request = app1.textRequest(msg,options);
-	var context='';
-	request.on('response',function(response){
-			var param = response.result.contexts[0].parameters;
-			speech = response.result.fulfillment.speech ;
-			if(response.result.metadata.intentName=='find_singer - custom' || response.result.metadata.intentName =='listen_song'){
-			if(response.result.metadata.intentName =='listen_song')
-				singer=param['singer.original'];
-			console.log('find_singer - custom' +singer);
-			
-			
-				var path='./song_list/'+singer+'.txt';
-				fs.readFile(path, function (err, data) {
-				if (err) 
-					console.log(err);
- 
-				var str=data.toString();
-				if(str.includes(param['song.original'])==true){
-				}else{
-					speech = '歌手沒有唱此首歌';	
+		var options = {
+			sessionId: uuid(),
+			contexts:[
+			  {
+				"name": "find_singer-followup",
+				"parameters": {
+				  "singer": singer,
+				  "singer.original": singer
 				}
-			event.reply(speech).then(function(data) {
-      // success 
-			console.log(response);
-			if(response.result.metadata.intentName=='find_singer'){
-			console.log('find_singer!');
-			singer=param['singer.original'];}
+				
+			  }
+			]
+		};		
+	}
+	
+	console.log('singers' + singer);
+	
+	// query information 
+	var request = app1.textRequest(msg,options);
+	
+	request.on('response',function(response){
+		
+			var param = response.result.contexts[0].parameters;
+			//get dialogflow's sentence
+			speech = response.result.fulfillment.speech ;
+			
+			//find_singer or listen_song
+			if(response.result.metadata.intentName=='find_singer - custom' || response.result.metadata.intentName =='listen_song'){
+				if(response.result.metadata.intentName =='listen_song')
+					singer=param['singer.original'];
+				console.log('find_singer - custom' +singer);
+				
+				//test context 
+				console.log(String(param));
+				var path='./song_list/'+singer+'.txt';
+					fs.readFile(path, function (err, data) {
+						if (err){ 
+							console.log(err);
+							speech = '沒有此歌手所唱歌曲資料';
+						}
+						var str=data.toString();
+						if(str.includes(param['song.original'])==true){
+						}else
+						{
+							speech = '歌手沒有唱此首歌';	
+						}
+							//line bot replay
+							event.reply(speech).then(function(data) {
+							// success 
+								console.log(response);
+								if(response.result.metadata.intentName=='find_singer'){
+								console.log('find_singer!');
+								singer=param['singer.original'];}
 
-			}).catch(function(error) {
-      // error 
-			console.log('error');
-			});
-			
-			});
-			
+							}).catch(function(error) {
+							// error 
+							console.log('error');
+							});			
+					});			
 			
 			}
 			else{
-		    event.reply(speech).then(function(data) {
-      // success 
-			console.log(response);
-			if(response.result.metadata.intentName=='find_singer'){
-			console.log('find_singer!');
-			singer=param['singer.original'];}
+				event.reply(speech).then(function(data) {
+				// success 
+				console.log(response);
+				if(response.result.metadata.intentName=='find_singer'){
+				console.log('find_singer!');
+				singer=param['singer.original'];}
 
-			}).catch(function(error) {
-      // error 
-			console.log('error');
-			});
+				}).catch(function(error) {
+				// error 
+				console.log('error');
+				});
 			}
 	});
 	request.on('error',function(error){
